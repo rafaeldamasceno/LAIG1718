4cfeded375181d3c56b9149f690eebccaab080b6
titarta <tiagorvcarvalho@gmail.com>
2017-10-06 18:26:05 +0100

    did the cilinder

--- MyCylinder.js
+++ MyCylinder.js
@@ -17,30 +17,44 @@ MyCylinder.prototype.constructor = MyCylinder;
 
 
 MyCylinder.prototype.initBuffers = function() {
-	// this.vertices = [];
- // 	this.normals = [];
- // 	this.texCoords = [];
- this.vertices = [
-   0, 1, 0,
-   1, 1, 0,
-   1, 0, 0,
-   0, 0, 0,
- ];
 
- this.indices = [
-   2, 1, 0,
-   1, 2, 3,
- ];
+this.vertices = [];
+this.normals = [];
+this.indices = [];
+//this.texCoords = [];
 
- this.normals = [
-   0, 0, 1,
-   0, 0, 1,
-   0, 0, 1,
-   0, 0, 1
- ];
+for(i = 0; i <= this.stacks; i++) {
+  var r = this.bottomRadius - (this.bottomRadius - this.topRadius)*(i/this.stacks);
+  for(j = 0; j <= this.slices; j++) {
 
+    var x = r*Math.cos(2*Math.PI*j/this.slices);
+    var y = r*Math.sin(2*Math.PI*j/this.slices);
+    this.vertices.push(x, y, this.height*i/this.stacks);
+
+    this.normals.push(x, y,(this.bottomRadius - this.topRadius)*r/this.height);
+
+
+
+  }
+}
+
+for(j = 0; j < this.stacks; j++) {
+  for(i = 0; i < this.slices; i++) {
+
+    this.indices.push(i + j*(this.slices + 1));
+    this.indices.push((i + 1)%this.slices + j*(this.slices + 1));
+    this.indices.push((i + 1)%this.slices + (j + 1)*(this.slices + 1));
+
+    this.indices.push(i + j*(this.slices + 1));
+    this.indices.push((i + 1)%this.slices + (j + 1)*(this.slices + 1));
+    this.indices.push(i + (j + 1)*(this.slices + 1));
+
+
+
+  }
+}
 
 
  	this.primitiveType = this.scene.gl.TRIANGLES;
  	this.initGLBuffers();
- };
+};
