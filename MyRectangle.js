fc923d14bd2b24eb8eb18ad1089362bb92ade680
rafaeldamasceno <rafaeldamasceno@me.com>
2017-10-07 12:19:37 +0100

    Rectangles are hard. I'm not sure what's wrong :/

--- MyRectangle.js
+++ MyRectangle.js
@@ -33,7 +33,7 @@ MyRectangle.prototype.initBuffers = function () {
     0, 0, 1
   ];
 
-  this.origTexCoords = [0, this.yf - this. yi, 0, 0, this.xf - this. xi, 0, this.xf - this. xi, this.yf - this. yi];
+  this.origTexCoords = [0, Math.abs(this.yf - this. yi), 0, 0, Math.abs(this.xf - this. xi), 0, Math.abs(this.xf - this. xi), Math.abs(this.yf - this. yi)];
 
   this.primitiveType=this.scene.gl.TRIANGLES;
 	this.initGLBuffers();
