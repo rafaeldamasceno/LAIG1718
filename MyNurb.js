8202c325c0feae30392bd73b31e91831f82d78a3
rafaeldamasceno <rafaeldamasceno@me.com>
2017-10-12 10:38:09 +0100

    Nurbs fully work!

--- MyNurb.js
+++ MyNurb.js
@@ -9,7 +9,7 @@
   var knots1 = this.getKnotsVector(degreeU);
   var knots2 = this.getKnotsVector(degreeV);
 
-	var nurbsSurface = new CGFnurbsSurface(degreeU, degreeV, knots1, knots2, controlvertexes);
+  var nurbsSurface = new CGFnurbsSurface(degreeU, degreeV, knots1, knots2, controlvertexes);
 
   getSurfacePoint = function(u, v) {
   return nurbsSurface.getPoint(u, v);
@@ -24,13 +24,13 @@ MyNurb.prototype.constructor = MyNurb;
 
 MyNurb.prototype.getKnotsVector = function(degree) {
 
-	var knotsAux = [];
-	for (var i=0; i<=degree; i++) {
-		knotsAux.push(0);
-	}
-	for (var i=0; i<=degree; i++) {
-		knotsAux.push(1);
-	}
+  var knotsAux = [];
+  for (var i=0; i<=degree; i++) {
+    knotsAux.push(0);
+  }
+  for (var i=0; i<=degree; i++) {
+    knotsAux.push(1);
+  }
 
   return knotsAux;
 };
